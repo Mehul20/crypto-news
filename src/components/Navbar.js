185cd40aa031b6ca1331e07402055a18cc900700
Surabhi7602 <sshetty72@gatech.edu>
2022-06-21 23:25:19 -0400

    navbar linking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
 
 export default function ButtonAppBar() {
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    // 👇️ navigate to /
+    navigate("/login");
+  };
+
+  const navigateSignUp = () => {
+    // 👇️ navigate to /
+    navigate("/signup");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,8 +36,12 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Signup</Button>
+          <Button color="inherit" onClick={navigateLogin}>
+            Login
+          </Button>
+          <Button color="inherit" onClick={navigateSignUp}>
+            Signup
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
