2ce454a801de95e21d9b73ab1939a1b3835c26cd
avirathtibrewala <avirath.tibrewala@getox.com>
2022-06-26 15:18:43 -0400

    tablewithoutlogin changes

--- src/components/TableWithouLogin.js
+++ src/components/TableWithouLogin.js
@@ -30,34 +30,91 @@ export default function TableWithoutLogin() {
   async function loop() {
     const querySnapshot = await getDocs(collection(db, "table"));
     console.log(querySnapshot);
-    querySnapshot.forEach((doc) => {
+    // querySnapshot.forEach((doc) => {
+    //   temp.push(
+    //     createData(
+    //       doc.data().Name,
+    //       doc.data().Upvotes,
+    //       doc.data().Description[0]
+    //     )
+    //   );
+    // });
+    for (let i = 0; i < 3; i++) {
+      console.log(querySnapshot.docs[i].data());
       temp.push(
         createData(
-          doc.data().Name,
-          doc.data().Upvotes,
-          doc.data().Description[0]
+          querySnapshot.docs[i].data().Name,
+          querySnapshot.docs[i].data().Upvotes,
+          querySnapshot.docs[i].data().Description[0]
         )
       );
-    });
+    }
     setRows(temp);
     temp = [];
     console.log(rows);
   }
 
   return (
+    // <div>
+    //   <TableContainer component={Paper}>
+    //     <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    //       <TableHead>
+    //         <TableRow>
+    //           <TableCell style={{ width: "50%" }}>Article Name</TableCell>
+    //           <TableCell style={{ width: "15%" }} align="center">
+    //             Upvotes
+    //           </TableCell>
+    //           <TableCell style={{ width: "35%" }} align="center">
+    //             Topic
+    //           </TableCell>
+    //         </TableRow>
+    //       </TableHead>
+    //       <TableBody>
+    //         {rows.map((row) => (
+    //           <TableRow
+    //             key={row.name}
+    //             sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    //           >
+    //             <TableCell component="th" scope="row">
+    //               {row.name}
+    //             </TableCell>
+    //             <TableCell align="center">
+    //               <div
+    //                 style={{
+    //                   display: "flex",
+    //                   alignItems: "center",
+    //                 }}
+    //               >
+    //                 <span>{row.link}</span>
+    //                 <ArrowCircleUpIcon onClick={loginAlertHandler} />
+    //               </div>
+    //             </TableCell>
+    //             <TableCell align="center">{row.desc}</TableCell>
+    //           </TableRow>
+    //         ))}
+    //       </TableBody>
+    //     </Table>
+    //   </TableContainer>
+    // </div>
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
+      <Table aria-label="simple table">
+        <TableHead className="tableheaddesign">
           <TableRow>
-            <TableCell style={{ width: "50%" }}>Article Name</TableCell>
-            <TableCell style={{ width: "15%" }} align="center">
-              Upvotes
+            <TableCell style={{ width: "50%" }} className="header">
+              <div className="header"> Article Name </div>
             </TableCell>
-            <TableCell style={{ width: "35%" }} align="center">
-              Topic
+            <TableCell style={{ width: "20%" }} align="left" className="header">
+              <div className="header"> Tags </div>
+            </TableCell>
+            <TableCell style={{ width: "15" }} align="left" className="header">
+              <div className="header"> Upvotes </div>
+            </TableCell>
+            <TableCell style={{ width: "15%" }} align="left" className="header">
+              <div className="header"> Link </div>
             </TableCell>
           </TableRow>
         </TableHead>
+
         <TableBody>
           {rows.map((row) => (
             <TableRow
@@ -65,8 +122,29 @@ export default function TableWithoutLogin() {
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                <a href={row.linking} target="_blank" className="text">
+                  {row.name}
+                </a>
               </TableCell>
+
+              <TableCell align="left">
+                {row.desc.map((data, index) => {
+                  return (
+                    <Chip
+                      label={data}
+                      variant="outlined"
+                      style={{
+                        backgroundColor: "#b79ced",
+                        color: "#fff",
+                        fontWeight: "bold",
+                        fontFamily: "Alice', serif",
+                      }}
+                      size="large"
+                    />
+                  );
+                })}
+              </TableCell>
+
               <TableCell align="center">
                 <div
                   style={{
@@ -78,7 +156,12 @@ export default function TableWithoutLogin() {
                   <ArrowCircleUpIcon onClick={loginAlertHandler} />
                 </div>
               </TableCell>
-              <TableCell align="center">{row.desc}</TableCell>
+              <TableCell>
+                <a href={row.linking} className="icon">
+                  {" "}
+                  <OpenInNewIcon> </OpenInNewIcon>{" "}
+                </a>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
